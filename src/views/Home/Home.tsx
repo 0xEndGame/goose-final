3d3ba43aa2ca0c17343cd29e9b4f4d1a61d86918
Merkel <you@example.com>
2022-02-23 21:23:04 +0100

    yolo3

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -23,7 +23,6 @@ const Hero = styled.div`
   text-align: center;
 
   ${({ theme }) => theme.mediaQueries.lg} {
-    background-image: url('/images/egg/merk_webp.webp'), url('/images/egg/merk_webp.webp');
     background-position: left center, right center;
     height: 165px;
     padding-top: 0;
